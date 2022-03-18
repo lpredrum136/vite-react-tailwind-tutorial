4e10b1a82f5fada5a94e09a425d544284dab6b8e
Henry Nguyen <nguyenhai1591@gmail.com>
2022-03-18 21:01:32 +1000

    done AuthBtn and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import AuthBtn from './components/AuthBtn'
+import Header from './components/Header'
 import Nav from './components/Nav'
 
 function App() {
@@ -6,16 +8,9 @@ function App() {
 		<div className='grid grid-cols-5'>
 			<Nav />
 
-			<main>
-				<h1>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-					architecto est quae modi ipsa debitis, consectetur provident
-					reprehenderit officia, error sed. Placeat vero, iusto enim eos tempore
-					dolorem consequuntur quisquam. Lorem ipsum dolor sit, amet consectetur
-					adipisicing elit. Totam nobis alias, sunt, dignissimos unde sed
-					veritatis delectus, ipsa recusandae omnis nam laboriosam reiciendis
-					cupiditate nemo explicabo obcaecati ut. Repellendus, sequi. lorem
-				</h1>
+			<main className='px-12 py-6 col-span-4 bg-cyan-50'>
+				<AuthBtn />
+				<Header />
 			</main>
 		</div>
 	)
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+	return (
+		<h1 className='text-cyan-900'>
+			#1 Free Online Movie Streaming Site in Vietnam
+		</h1>
+	)
+}
+
+export default Index
